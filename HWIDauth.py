39678f83c8662c74debe06752531a7d2fc3fa1c3
Anapah <70201574+reactxsw@users.noreply.github.com>
2021-07-16 06:59:23 +0700

    Update HWIDauth.py

--- HWIDauth.py
+++ HWIDauth.py
@@ -1,6 +1,6 @@
-import subprocess,requests,os
+import subprocess,requests
 try:r = requests.get('https://raw.githubusercontent.com/reactxsw/hwiddump/main/hwid.txt')
-except (requests.ConnectionError, requests.Timeout) as Exception:print('Error : Internet connection');os.system("pause")
+except (requests.ConnectionError, requests.Timeout) as Exception:print('Error : Internet connection');input()
 def check():return True if subprocess.check_output('wmic csproduct get uuid' ,shell=False).decode().split('\n')[1].strip() in r.text else False
-def main():print("Permission granted" if check() else "Permission denial")
+def main():print("Permission granted" if check() else "Permission denial");input()
 main()
